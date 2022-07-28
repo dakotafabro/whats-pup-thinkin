6ce6535665137e1a8bf5c84f6ac386dcae63bcc9
dakotafabro <dakotafabrocgu@gmail.com>
2022-07-27 20:37:09 -0700

    added BackToTop component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Home from "./components/Home";
 import GalleryAllPups from "./components/GalleryAllPups";
 import SinglePupView from "./components/SinglePupView";
 import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route exact path="/submit" element={<SubmitPupPic />} />
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/gallery" element={<GalleryAllPups />} />
-        <Route exact path="/pup/:id" element={<SinglePupView />} />
-      </Routes>
+      <BackToTop>
+        <Routes>
+          <Route exact path="/submit" element={<SubmitPupPic />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/gallery" element={<GalleryAllPups />} />
+          <Route exact path="/pup/:id" element={<SinglePupView />} />
+        </Routes>
+      </BackToTop>
       <Footer />
     </div>
   );
